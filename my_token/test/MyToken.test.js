bc9efe107941c1db02f51bf702cf71eb82657551
Bhargav Naik <bhargavnaik07@gmail.com>
2022-02-17 22:59:24 -0600

    MyToken unit test to check transfer of ether

--- my_token/test/MyToken.test.js
+++ my_token/test/MyToken.test.js
@@ -7,5 +7,15 @@ contract("myToken", accounts => {
 
     it('give 1M to caller', async () => {
         let balance = await token.balanceOf(accounts[0])
+        balance = web3.utils.fromWei(balance, 'ether')
+        assert.equal(balance, 1000000, 'balance should be 1M')
+    });
+
+    it('transfer 1000 token to account1', async () => {
+        let amount = web3.utils.toWei('1000', 'ether')
+        await token.transfer(accounts[1], amount, {from: accounts[0]})
+        let balance = await token.balanceOf(accounts[1])
+        balance = web3.utils.fromWei(balance, 'ether')
+        assert.equal(balance, 1000, 'account1 balance should be 1000')
     });
 })
